6c0d6adfa646e8670ae570156409f51745f06c71
Zellenon <zellenon@gmail.com>
2025-05-22 22:12:34 -0500

    made things a bit more direct

--- 1/main.cpp
+++ 1/main.cpp
@@ -8,14 +8,13 @@ public:
   vector<int> twoSum(vector<int> &nums, int target) {
     std::unordered_map<int, int> hmap = {};
     for (int i = 0; i < nums.size() - 1; i++) {
-      int x = nums[i];
-      int remainder = target - x;
+      int remainder = target - nums[i];
       if (hmap.contains(remainder)) {
         return {i, hmap.at(remainder)};
       } else {
-        hmap.insert({x, i});
+        hmap.insert({nums[i], i});
       }
     }
-    return {-1, -1};
+    return {};
   }
 };
